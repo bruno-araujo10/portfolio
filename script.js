f4a8e17de695375aba3fed2be887de1d0132a0ae
bruno-araujo10 <brunoaraujo.developer@gmail.com>
2023-05-02 09:13:12 -0300

    scripts

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const titulo = document.querySelector('h1');
+let isPurple = false;
 
+const aparecer = () => {
+  if (!isPurple) {
+    titulo.style.color = '#7A1CBD';
+    isPurple = true;
+  }
+}
+
+const desaparecer = () => {
+  if (isPurple) {
+    titulo.style.color = 'white';
+    isPurple = false;
+  }
+}
+
+setInterval(() => {
+  aparecer();
+  setTimeout(desaparecer, 500);
+}, 1000);
